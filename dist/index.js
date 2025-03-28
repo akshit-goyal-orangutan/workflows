464d1edc38ded61a569d21608484b42a3011a0c8
Askhit Goyal <akshit.goyal@orangutantechnologies.com>
2025-03-28 07:08:31 +0000

    ignored build folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var http = require("node:http");
-var handler = function (req, res) {
-    res.end("Hello World");
-};
-var server = http.createServer(handler);
-server.listen(8000);
-//# sourceMappingURL=index.js.map
