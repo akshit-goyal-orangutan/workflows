322afe6e9c92ed8b04b535749dbcdaf43c47b13d
Askhit Goyal <akshit.goyal@orangutantechnologies.com>
2025-03-28 07:56:48 +0000

    lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import * as http from 'node:http'
+import * as http from 'node:http';
 
 const handler = (req: http.IncomingMessage, res: http.ServerResponse) => {
-    res.end("Hello World")
-}
+    res.end("Hello World");
+};
 
-const server = http.createServer(handler)
+const server = http.createServer(handler);
 
-console.log("Checkout http://localhost:8000")
-server.listen(8000)
+console.log("Checkout http://localhost:8000");
+server.listen(8000);
